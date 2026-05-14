15ec1a8e0f966e80665160202ed0d074c771a904
Noman Akram <noman.akram@cogentlabs.co>
2026-05-14 15:17:41 +0500

    added first and last name in admin

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,7 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ['email', 'phone_number', 'city', 'is_verified', 'is_staff']
+    list_display = ['first_name','last_name','email', 'phone_number', 'city', 'is_verified', 'is_staff']
     search_fields = ['email',]
     readonly_fields = ('created_at', 'updated_at')
 
