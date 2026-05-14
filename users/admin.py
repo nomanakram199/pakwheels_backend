8b14792d6de433ac19bb750d7f0e4d0a7787217d
Noman Akram <noman.akram@cogentlabs.co>
2026-05-14 14:28:41 +0500

    added the auth functionality

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'phone_number', 'city', 'is_verified', 'is_staff']
+    search_fields = ['email',]
+    readonly_fields = ('created_at', 'updated_at')
+
